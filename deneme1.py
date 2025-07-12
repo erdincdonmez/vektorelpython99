5b3f475bc23d12a4724c0f9d20e35c868e589e3a
erdincdonmez <erdinc_donmez@yahoo.com>
2025-07-12 11:02:44 +0300

    2.hafta,1.gün github yükleme

---
+++ deneme1.py
@@ -0,0 +1,1 @@
+print("Git huba gönderme.")
---
+++ h2_3 if/input tekrar1.py
@@ -0,0 +1,7 @@
+# sayi1 = input("1.Sayıyı gir:")
+# sayi2 = input("2.Sayıyı gir:")
+# print("Toplam: ",sayi1+sayi2)
+
+sayi1 = int(input("1.Sayıyı gir:"))
+sayi2 = int(input("2.Sayıyı gir:"))
+print("Toplam: ",sayi1+sayi2)
